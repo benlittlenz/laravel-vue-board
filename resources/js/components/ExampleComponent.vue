4c2057df3c5458da3654c12e3376213071324a70
nzbenji <benlittle53@gmail.com>
2020-03-31 08:03:27 +1300

    added sanctum

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="">
+        {{ user }}
     </div>
 </template>
 
 <script>
+import axios from 'axios'
     export default {
+        data() {
+            return {
+                user: null
+            }
+        },
+        methods: {
+            async getUser() {
+                let res = await axios.get('/api/user')
+
+                this.user = res.data
+            }
+        },
         mounted() {
-            console.log('Component mounted.')
+            this.getUser()
         }
     }
 </script>
