72de6fe475846bc916c3d42ba3c69f815bb8f754
nzbenji <benlittle53@gmail.com>
2020-04-01 18:41:10 +1300

    setup project with user relation

--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use App\User;
+use App\Project;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -36,4 +38,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function projects() {
+        return $this->BelongsToMany(Project::class);
+    }
 }
